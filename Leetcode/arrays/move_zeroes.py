c68c6a69369492198d6fd75463714b5a74263f83
unknown <akansha.dakre@gmail.com>
2022-07-11 22:34:42 +0530

    initial commit

---
+++ Leetcode/arrays/move_zeroes.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        #similar to quick sort logic
+        l=0
+        for r in range(0,len(nums)):
+            if nums[r]:
+                #swap two numbers
+                nums[r],nums[l]=nums[l],nums[r] 
+                l+=1
+
+        return nums
+                    
+                    
+        
